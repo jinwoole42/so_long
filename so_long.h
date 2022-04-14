3102d01a17eb438cc2e3da1edda4f5f51c7c0458
Jinwoo Lee <jinwoole@c4r1s5.42seoul.kr>
2022-04-14 16:34:47 +0900

    get_next_line

--- so_long.h
+++ so_long.h
@@ -1,24 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   so_long.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jinwoole <indibooks@naver.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/12 18:03:42 by jinwoole          #+#    #+#             */
-/*   Updated: 2022/04/14 16:22:41 by jinwoole         ###   ########.fr       */
+/*   Created: 2022/04/14 16:17:10 by jinwoole          #+#    #+#             */
+/*   Updated: 2022/04/14 16:20:44 by jinwoole         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "so_long.h"
-#include "./get_next_line/get_next_line.h"
+#ifndef SO_LONG_H
+# define SO_LONG_H
 
-int main(void)
-{
-	void *mlx_ptr;
-	void *win_ptr;
+#include "mlx.h"
 
-	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "mlx 42");
-	mlx_loop(mlx_ptr);
-}
+#endif
