f513b60c6aad4d3c208807bcd1985f4af8650a48
Jinwoo Lee <jinwoole@c4r3s6.42seoul.kr>
2022-05-10 20:11:42 +0900

    do

--- main/main.c
+++ main/main.c
@@ -6,11 +6,23 @@
 /*   By: jinwoole <indibooks@naver.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 18:03:42 by jinwoole          #+#    #+#             */
-/*   Updated: 2022/05/10 20:00:27 by jinwoole         ###   ########.fr       */
+/*   Updated: 2022/05/10 20:10:20 by jinwoole         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../so_long.h"
+
+void	map_check(int argc, char **argv, t_map *map)
+{
+	char	*map_path;
+
+	map_path = argv[1];
+	name_error(argc, argv);
+	shape_check(map_path);
+	map = malloc(sizeof(t_map));
+	map_info(map_path, map);
+	data_check(map);
+}
 
 int	main(int argc, char **argv)
 {
@@ -18,31 +30,12 @@ int	main(int argc, char **argv)
 	void	*mlx_win;
 	void	*img;
 	char	*char_path = "./src/finn.xpm";
-	char	*map_path;
 	int		img_width;
 	int		img_height;
 	t_map	*map;
 
-	map_path = argv[1];
-	name_error(argc, argv);
-	shape_check(map_path);
-	map = malloc(sizeof(t_map));
-	map_info(map_path, map);
-
-	int i;
-
-	i = 0;
-	printf("%d\n", map->width);
-	printf("%d\n", map->height);
-	while (i < map->height)
-	{
-		printf("%s\n", map->data[i]);
-		i++;
-	}
-
-	data_check(map);
-
-	
+	map = 0;
+	map_check(argc, argv, map);
 	mlx = mlx_init();
 	mlx_win = NULL;
 	mlx_win = mlx_new_window(mlx, 1280, 720, "finn");
